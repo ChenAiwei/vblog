7432c13eddcabe5b9cc674771b113ebfd7d3c73a
ChenAiwei <2268967541@qq.com>
2020-03-20 14:14:09 +0800

    project init

---
+++ pages/classification/classification.vue
@@ -0,0 +1,30 @@
+<template>
+	<view class="content">
+	分类
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content{
+		width: 100%;
+		height: 100vh;
+		background-color: #272725;
+		color: #D6D0C4;
+	}
+</style>
