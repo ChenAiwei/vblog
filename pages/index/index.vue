7cfdf4bfeb564120c565566b83cdff9b3487b1e9
ChenAiwei <2268967541@qq.com>
2020-03-20 16:49:20 +0800

    项目重构

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,30 +1,47 @@
 <template>
-	<view class="content">
-		首页
-	</view>
+	<view class="content">首页</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-			
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+export default {
+	data() {
+		return {
+			home: [
+				{
+					tagId: 1,
+					count: 2,
+					tag: 'Google',
+					yearList: [
+						{
+							articleList: [
+								{
+									view: 15,
+									createTime: 1584693927099,
+									like: 1,
+									subtitle: 'talk how to use',
+									comment: 0,
+									id: 1,
+									title: 'Google mail trans as diff '
+								}
+							],
+							year: '2020',
+							id: 1
+						}
+					]
+				}
+			]
+		};
+	},
+	onLoad() {},
+	methods: {}
+};
 </script>
 
 <style lang="scss">
-	.content{
-		width: 100%;
-		height: 100vh;
-		background-color: #272725;
-		color: #D6D0C4;
-	}
+.content {
+	width: 100%;
+	height: 100vh;
+	background-color: #272725;
+	color: #dfd8ca;
+}
 </style>
